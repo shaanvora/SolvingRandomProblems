edf60f2a92b4484059b3fe3ba458ac3fed5c7e33
shaanvora <63378339+shaanvora@users.noreply.github.com>
2020-05-19 09:39:37 -0500

    Initial Version

--- staircaseproblem.py
+++ staircaseproblem.py
@@ -1,4 +1,6 @@
 #recursive solution to determining the number of way you can ascend a staircase with a give number of steps and a list of step sizes
+#steps is the number of steps in the staircase
+#possiblesteps is a list of step sizes to be taken
 
 def num_steps(steps, possiblesteps):
     if steps == 0:
